d14c27e4d9cdab3f26ce7210de8ce70e0b83b6c8
Chin Wing Lam <bearkitty18@gmail.com>
2024-03-20 15:45:42 +0000

    API import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React, { useEffect } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Header from './components/header/header';
 import HomePage from './pages/home/home';
@@ -12,7 +12,7 @@ import WindSpeedPage from "./pages/Wind/WindSpeed";
 import UVPage from "./pages/UV/UV";
 import DayPage from "./pages/Days/Days";
 
-import { extractWeather, fetchByCity } from './functions/weather'
+import { extractWeather, fetchByCity } from './functions/weather';
 import { useGlobalState } from './stores/weatherState';
 
 
@@ -21,67 +21,66 @@ function NoPage() {
 }
 
 function App() {
-  const [state, dispatch] = useGlobalState()
+  const [state, dispatch] = useGlobalState();
 
   // init block to retrieve the data
   useEffect(() => {
     async function init() {
-      const json = await fetchByCity("London")
-      dispatch({isLoading: false, json})
+      const json = await fetchByCity("London");
+      dispatch({ isLoading: false, json });
     }
-    init()
-  }, [])
+    init();
+  }, []);
 
   // update the weather when state.json changes
   useEffect(() => {
-    setBackground(extractWeather(state.json))
-  }, [state.json])
+    setBackground(extractWeather(state.json));
+  }, [state.json]);
 
   function setBackground(weather) {
-    console.log(weather)
-
-    // decide which background to use
     const app = document.getElementsByClassName('App')[0];
     let backgroundImg;
 
-    if (weather.main === "Clear") {
-      backgroundImg = "./backgrounds/clearsky.png"
-    }
-    else if (weather.main === "Rain") {
-      backgroundImg = "./backgrounds/rainyDay.png"
-    }
-    else if (weather.main === "Snow") {
-      backgroundImg = "./backgrounds/Snow.png"
-    }
-    else if (weather.main === "Clouds") {
-      backgroundImg = "./backgrounds/cloudySky.png"
-    }
+    if (weather && weather.weatherConditions && weather.weatherConditions.main) {
+      if (weather.weatherConditions.main === "Clear") {
+        backgroundImg = "./backgrounds/clearsky.png";
+      }
+      else if (weather.weatherConditions.main === "Rain") {
+        backgroundImg = "./backgrounds/rainyDay.png";
+      }
+      else if (weather.weatherConditions.main === "Snow") {
+        backgroundImg = "./backgrounds/Snow.png";
+      }
+      else if (weather.weatherConditions.main === "Clouds") {
+        backgroundImg = "./backgrounds/cloudySky.png";
+      }
 
-    // set background
-    app.style.backgroundImage = "url(\"" + backgroundImg + "\")"
+      if (backgroundImg) {
+        app.style.backgroundImage = `url(${backgroundImg})`;
+      }
+    }
   }
 
   return (
-    <div className="App">
-      <Header/>
+      <div className="App">
+        <Header />
 
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<HomePage/>}/>
-          <Route path="/temperature" element={<TemperaturePage/>}/>
-          <Route path="/precipitation" element={<PrecipitationPage/>}/>
-          <Route path="/humidity" element={<HumidityPage/>}/>
-          <Route path="/AQI" element={<AQIPage/>}/>
-          <Route path="/wind" element={<WindSpeedPage/>}/>
-          <Route path="/uv" element={<UVPage/>}/>
-          <Route path="/Day" element={<DayPage/>}/>
-          <Route path='*' element={<NoPage/>}/>
-        </Routes>
-      </BrowserRouter>
-    </div>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<HomePage />} />
+            <Route path="/temperature" element={<TemperaturePage />} />
+            <Route path="/precipitation" element={<PrecipitationPage />} />
+            <Route path="/humidity" element={<HumidityPage />} />
+            <Route path="/AQI" element={<AQIPage />} />
+            <Route path="/wind" element={<WindSpeedPage />} />
+            <Route path="/uv" element={<UVPage />} />
+            <Route path="/Day" element={<DayPage />} />
+            <Route path='*' element={<NoPage />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
   );
 }
 
-
-
 export default App;
+
--- src/functions/weather.js
+++ src/functions/weather.js
@@ -1,14 +1,28 @@
 const token = "b787f8f939e6de8235f3e80d9aac65ba"
 
 export async function fetchByCity(city) {
-    const res = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${token}`)
-    return res.json()
+    try {
+        const res = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${token}&units=metric`);
+        return res.json();
+    } catch (error) {
+        console.error("Error fetching weather data:", error);
+        return null;
+    }
 }
 
 export function extractWeather(json) {
-    if (json === undefined) {
-        return 'undefined'
+    if (!json || !json.list || json.list.length === 0) {
+        return null;
     }
 
-    return json.list[0].weather[0]
+    const firstForecast = json.list[0];
+    const dateTime = firstForecast.dt_txt.split(" ");
+
+    return {
+        date: dateTime[0],
+        time: dateTime[1],
+        maxTemperature: firstForecast.main.temp_max,
+        minTemperature: firstForecast.main.temp_min,
+        weatherConditions: firstForecast.weather[0]
+    };
 }
