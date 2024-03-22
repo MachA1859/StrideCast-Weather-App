d97bb2e6f7ff7f3632992a403ac87e6575a68aaa
Chin Wing Lam <bearkitty18@gmail.com>
2024-03-22 18:28:20 +0000

    remove uncessarys

--- src/functions/weather.js
+++ src/functions/weather.js
@@ -1,37 +1,23 @@
 const token = "b787f8f939e6de8235f3e80d9aac65ba"
 
 export async function fetchByCity(city) {
-    try {
-        const res = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${token}&units=metric`);
-        return res.json();
-    } catch (error) {
-        console.error("Error fetching weather data:", error);
-        return null;
-    }
+    const res = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${token}`)
+    return res.json()
 }
 
 export function extractWeather(json) {
-    if (!json || !json.list || json.list.length === 0) {
-        return [];
+    if (json === undefined) {
+        return 'undefined'
     }
 
-    return json.list.map(forecast => {
-        const dateTime = forecast.dt_txt.split(" ");
+    return json.list[0].weather[0]
+}
 
-        return {
-            date: dateTime[0],
-            time: dateTime[1],
-            maxTemperature: forecast.main.temp_max,
-            minTemperature: forecast.main.temp_min,
-            humidity: forecast.main.humidity,
-            weatherConditions: {
-                main: forecast.weather[0].main,
-                description: forecast.weather[0].description,
-                icon: forecast.weather[0].icon,
-            },
-            pop: forecast.pop,
-            city: json.city.name
-        };
-    });
+export function extractCity(json) {
+    if (json === undefined) {
+        return 'undefined'
+    }
+
+    return json.city.name
 }
 
