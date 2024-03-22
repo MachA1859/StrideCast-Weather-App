d4d44c2cbd7bf6a07ca6c9e3b89c004fd92290aa
Chin Wing Lam <bearkitty18@gmail.com>
2024-03-22 01:29:07 +0000

    Suggestions

--- src/pages/precipitation/precipitation.js
+++ src/pages/precipitation/precipitation.js
@@ -1,27 +1,66 @@
-import { Ribbon } from "../../components/ribbon/ribbon";
+import React, { useEffect, useState } from 'react';
+import {useGlobalState} from "../../stores/weatherState";
+import {Ribbon} from "../../components/ribbon/ribbon";
+import Forecast from "../../components/forecast/forecast";
 import Card from "../../components/card/card";
 import RainChart from "./Rainchart";
-import Forecast from "../../components/forecast/forecast";
-
-
 
 export default function PercipitationPage() {
-    return (
-        <>
-            <Ribbon/>
+    const [weatherData, setWeatherData] = useState(null);
+    const [weather] = useGlobalState();
+
+    useEffect(() => {
+        if (weather.json === undefined) {
+            setWeatherData([])
+
+        } else {
+            const today = []
+            for (let i = 0; i < weather.json.list.length; i++) {
+                const dt = new Date(weather.json.list[i].dt_txt)
+                const now = new Date()
+                if (now.getDate() === dt.getDate())
+                    today.push(weather.json.list[i])
+            }
+            console.log(today)
+            setWeatherData(today)
+        }
+
+    }, [weather.json])
+
+    let rainForecast = "";
+    let bringClothes = false;
+    if (weatherData) {
+        for (let i = 0; i < weatherData.length; i++) {
+            const dt = new Date(weatherData[i].dt_txt);
+            const time = `${dt.getHours()}`;
+            if (weatherData[i].pop >= 0.2 && weatherData[i].pop < 0.5) {
+                rainForecast += `At ${time}, there is a small chance of rain.\n`;
+                bringClothes = true;
+            } else if (weatherData[i].pop >= 0.5 && weatherData[i].pop < 1) {
+                rainForecast += `At ${time}, there is a moderate chance of rain.\n`;
+                bringClothes = true;
+            } else if (weatherData[i].pop === 1) {
+                rainForecast += `At ${time} 0, there is a high chance of rain.\n`;
+                bringClothes = true;
+            }
+        }
+        if (bringClothes) {
+            rainForecast += "Remember to bring dry clothes to change.\n";
+        }
 
 
-            <Card>
-                <RainChart/>
-            </Card>
+        return (
+            <>
+                <Ribbon/>
 
-            <Forecast
-                today={{
-                    'hi': 10,
-                    'low': 3
-                }}
-                suggestions={'hi'}
-            />
-        </>
-    )
+                <Card>
+                    <RainChart/>
+                </Card>
+
+                <Forecast
+                    suggestions={rainForecast}
+                />
+            </>
+        )
+    }
 }
