464525a81268baf109adc8b7bc17c4666bb5a4a2
Chin Wing Lam <bearkitty18@gmail.com>
2024-03-22 01:44:05 +0000

    humid suggest

--- src/pages/humidity/humidity.js
+++ src/pages/humidity/humidity.js
@@ -1,8 +1,45 @@
+import React, { useEffect, useState } from 'react';
+import {useGlobalState} from "../../stores/weatherState";
 import {Ribbon} from "../../components/ribbon/ribbon";
 import Forecast from "../../components/forecast/forecast";
 import Card from "../../components/card/card";
 import HumidityChart from "./Humiditychart";
+
 export default function HumidityPage() {
+    const [weatherData, setWeatherData] = useState(null);
+    const [weather] = useGlobalState();
+
+    useEffect(() => {
+        if(weather.json===undefined){
+            setWeatherData([])
+
+        }
+        else {
+            const today=[]
+            for(let i=0;i<weather.json.list.length;i++){
+                const dt= new Date(weather.json.list[i].dt_txt)
+                const now= new Date()
+                if(now.getDate()===dt.getDate())
+                    today.push(weather.json.list[i])
+            }
+            console.log(today)
+            setWeatherData(today)
+        }
+
+    }, [weather.json])
+
+    // Assuming the current humidity is the first item in the weatherData array
+    const currentHumidity = weatherData && weatherData.length > 0 ? weatherData[0].main.humidity : null;
+
+    let suggestions = "";
+    if (currentHumidity > 0.6) {
+        suggestions = "Today has high humidity";
+    } else if (currentHumidity > 0.4) {
+        suggestions = "Today has moderate humidity, best for running";
+    } else if (currentHumidity <= 0.2) {
+        suggestions = "It is dry, take care";
+    }
+
     return (
         <>
             <Ribbon/>
@@ -12,12 +49,8 @@ export default function HumidityPage() {
             </Card>
 
             <Forecast
-                today={{
-                    'hi': 10,
-                    'low': 3
-                }}/>
-
-
+                suggestions={suggestions}
+            />
         </>
     )
 }
